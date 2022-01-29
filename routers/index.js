74ec5818c9a0814770e6afa48b57c2f6ee89d8cb
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2022-01-29 18:34:14 -0500

    finalizacion de platilla para modelo hexagonal

---
+++ routers/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+//const firebaserouter = require('../services/firebase/router.js');
+
+
+//router.post('/registertokentopic', firebaserouter.registerintopic);
+
+
+
+module.exports = router;
